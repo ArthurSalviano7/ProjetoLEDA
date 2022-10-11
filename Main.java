7e69437e031297293fd2fdfc3dbfeb0a6e346ab6
Arthur Salviano <arthursalviano7@gmail.com>
2022-10-11 15:27:41 -0300

    criação arquivo Main

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class lerCSV{
+public class Main{
 
     public static void main(String[] args) {
         
